5a8a99b5e0431ed6dc0e8c25bfefd9784a4e6612
Emilio Guarino <emilio.guarino@gmail.com>
2022-02-09 22:05:45 -0500

    added page loader

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
-// site load animations
-gsap.from(".hero", {duration: 1, x:-100, opacity: 0.01, delay:1, scale: 0.8,});
-gsap.from(".site-headline", {duration: 2, x: 0, opacity: 1, scale: 0.98 });
 
-//emilioguarino nav animation
-gsap.from(".the-name", {duration: 1.5, x:0, y:-100,  ease: "back.inOut(1.7)"});
-gsap.from(".the-name", {duration: 1, opacity: 0.1, delay: 0.5,})
+gsap.from("#loader-area", {duration: 1, opacity: 0})
+gsap.from(".loader", {duration: 1, opacity: 0})
 
-//headline breathing loop
-gsap.to(".site-headline", {duration: 2, opacity: .95, scale: 0.92, repeat: -1,ease: "power1.inOut", yoyo: true, yoyoEase: true});
 
-TweenMax.to("html", 1, {"--myColor":"orange", yoyo:true, repeat:20});
 
-window.onscroll = () => { 
-    TweenMax.to("html", 1, {"--myText":"1",})
-    
+//this function changes CSS opacity for content area to make it visible
+
+function pageVisible() {
+    const ele = document.getElementById("entire-page")
+    ele.style.opacity = '1'
 }
+
+window.addEventListener('load', (event) => {
+    pageVisible()
+    
+    //fades out loader
+        gsap.to(".loader", {duration: 1, opacity: 0})
+        gsap.to("#loader-area", {duration: 1, opacity: 0})
+
+    // site load animations
+        gsap.from(".hero", {duration: 1, opacity: 0.01, delay:1, scale: 0.8,});
+        gsap.from(".site-headline", {duration: 2, x: 0, opacity: 1, scale: 0.98 });
+
+
+    //emilioguarino nav animation
+        gsap.from(".the-name", {duration: 1.5, x:0, y:-100,  ease: "back.inOut(1.7)"});
+        gsap.from(".the-name", {duration: 1, opacity: 0.1, delay: 0.5,})
+    
+    //headline breathing loop
+        gsap.to(".site-headline", {duration: 2, opacity: .95, scale: 0.92, repeat: -1,ease: "power1.inOut", yoyo: true, yoyoEase: true});
+
+        TweenMax.to("html", 1, {"--myColor":"orange", yoyo:true, repeat:20});
+
+        window.onscroll = () => { 
+            TweenMax.to("html", 1, {"--myText":"1",})
+            
+}
+
+});
+
+
+
+
+
+
+
 
 
