51fa99538fd617fae048c2828fe5ec029b52bcdb
Emilio Guarino <emilioguarino@Emilios-MBP.localdomain>
2022-10-13 00:39:28 -0400

    finished header and animation

--- portfolio-2/components/Header.tsx
+++ portfolio-2/components/Header.tsx
@@ -22,13 +22,33 @@ function Header({}: Props) {
             opacity: 1,
             scale: 1
           }}
+
+          transition={{
+            duration: 1
+          }}
           className="flex flex-row items-center">
             <SocialIcon url='https://www.linkedin.com/in/emilio-guarino-99183a1b1/' fgColor="grey" bgColor="transparent"> </SocialIcon>
             <SocialIcon url='https://github.com/mlgrn'fgColor="grey" bgColor="transparent"> </SocialIcon>
             <SocialIcon url='https://youtube.com/'fgColor="grey" bgColor="transparent"> </SocialIcon>
           </motion.div>
 
-          <div>
+          <motion.div
+          initial={{
+            x: 500,
+            opacity: 0,
+            scale: 0.5
+          }}
+
+          animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1
+          }}
+
+          transition={{
+            duration: 1
+          }}
+          >
             
           <SocialIcon 
           className='cursor-pointer'
@@ -36,7 +56,7 @@ function Header({}: Props) {
           fgColor='grey'
           bgColor='transparent'/>
             <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>email me here</p>
-          </div>
+          </motion.div>
         </header>
 
     </div>
