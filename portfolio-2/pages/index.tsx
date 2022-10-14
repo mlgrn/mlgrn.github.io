170ae1d56078775a8bbb4b41f9786aa42e6970ee
Emilio Guarino <emilioguarino@Emilios-MBP.localdomain>
2022-10-13 23:57:20 -0400

    hero animation work

--- portfolio-2/pages/index.tsx
+++ portfolio-2/pages/index.tsx
@@ -3,15 +3,21 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
+
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen">
       <Head>
         <title>Emilio Guarino</title>
       </Head>
 
         <Header />
+
+        <section id="hero">
+          <Hero />
+        </section>
     </div>
   )
 }
