a0b7d3115cf13c24f64aaca082685d1507a3b69d
Emilio Guarino <emilioguarino@Emilios-MBP.localdomain>
2022-10-13 23:58:41 -0400

    hero animation

---
+++ portfolio-2/components/Hero.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
+import BackgroundCircles from './BackgroundCircles'
+
+
+type Props = {}
+
+function Hero({}: Props) {
+const [text, count] = useTypewriter({
+    words: ["My name is Emilio.", "Musician", "Developer", "Video Producer"],
+    loop: true,
+    delaySpeed: 2000
+    
+})
+
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center">
+            <BackgroundCircles />
+        <h1>
+            <span>{text}</span>
+            <Cursor cursorColor='#9A21C6'/>
+        </h1>
+    
+    </div>
+  )
+}
+
+export default Hero
