ac84b034306b60e5dfcb8e37442cac62426f7a93
Emilio Guarino <emilioguarino@Emilios-MacBook-Pro.local>
2022-10-17 01:54:11 -0400

    added image, need to style it

--- portfolio-2/components/Hero.tsx
+++ portfolio-2/components/Hero.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import BackgroundCircles from './BackgroundCircles'
+import Image from 'next/image'
+import EmilioHero from "../images/HotWobble-2.jpg"
 
 
 type Props = {}
@@ -14,14 +16,24 @@ const [text, count] = useTypewriter({
 })
 
   return (
+
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
-            <BackgroundCircles />
+            
+    <BackgroundCircles />
+    {/* figure out why I can't get the rounded class to work here */}
+          <div className="relative w-80 h-80 mx-auto object-cover">
+            <Image src={EmilioHero} alt="picture of emilio" layout="fill" />
+          </div>
+           
+          
         <h1>
             <span>{text}</span>
             <Cursor cursorColor='#9A21C6'/>
         </h1>
     
     </div>
+
+    
   )
 }
 
