50aca407238244dec3145b8b450efd8fa47f1bf1
EG <laptop@EGs-MacBook-Pro.local>
2025-08-14 18:58:40 -0400

    Switch to Vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const base = '';
@@ -13,7 +13,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({ fallback: '404.html' }),
+		adapter: adapter(),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? base : ''
 		}
