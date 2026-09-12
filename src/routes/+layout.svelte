7e4d0403515d360cbfe4139aa7b1a1120850f139
EG <laptop@EGs-MacBook-Pro.local>
2026-09-11 21:05:30 -0400

    added vercel analytics and meta conv API

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	import NavBar from '$lib/components/common/nav-bar/nav-bar.svelte';
 	import 'virtual:uno.css';
 	import '../app.css';
 	import '../markdown.css';
 	import { ModeWatcher } from 'mode-watcher';
 
-	let { children } = $props();
-	
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 
-	
+	let { children } = $props();
 </script>
 
 <ModeWatcher />
